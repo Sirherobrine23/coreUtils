97f77817ad9e81dfd7ffab84d52ef5e3e94ec5d8
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-15 17:46:34 -0300

    Update simples.ts

--- src/request/simples.ts
+++ src/request/simples.ts
@@ -10,6 +10,7 @@ async function gotCjs(): Promise<(typeof import("got"))["default"]> {
     http2: true,
     headers: {
       "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
       "Accept": "*/*"
     }
   });
