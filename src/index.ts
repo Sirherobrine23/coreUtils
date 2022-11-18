4f081a4b73b91efebaecf078eaf91b23450cf846
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-18 17:18:04 +0000

    update exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export * as httpRequest from "./request/simples";
-export * as httpRequestLarge from "./request/large";
-export * as httpRequestGithub from "./request/github";
-export * as httpRequestClient from "./request/client";
-export * as extendFs from "./extendsFs";
 export * as customChildProcess from "./childPromisses";
+export * as extendFs from "./extendsFs";
+// Imports http
+import * as httpRequest from "./request/simples";
+import * as httpRequestLarge from "./request/large";
+import * as httpRequestGithub from "./request/github";
+import * as httpRequestClient from "./request/client";
+export {httpRequest, httpRequestClient, httpRequestLarge, httpRequestGithub};
+export const requests = {
+  client: httpRequestClient,
+  github: httpRequestGithub,
+  ...httpRequest,
+  ...httpRequestLarge,
+}
