3d7339ef811fe1acec37299e2b5de4fdd68d3f54
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2023-01-22 11:33:24 -0300

    Add dot notation

---
+++ src/notation.ts
@@ -0,0 +1,60 @@
+// Mongo notation
+type NestedPaths<Type, Depth extends number[]> = Depth['length'] extends 8 ? [] : Type extends string | number | boolean | Date | RegExp | Buffer | Uint8Array | ((...args: any[]) => any)  ? [] : Type extends ReadonlyArray<infer ArrayType> ? [] | [number, ...NestedPaths<ArrayType, [...Depth, 1]>] : Type extends Map<string, any> ? [string] : Type extends object ? {
+    [Key in Extract<keyof Type, string>]: Type[Key] extends Type ? [Key] : Type extends Type[Key] ? [Key] : Type[Key] extends ReadonlyArray<infer ArrayType> ? Type extends ArrayType ? [Key] : ArrayType extends Type ? [Key] : [
+    Key,
+    ...NestedPaths<Type[Key], [...Depth, 1]>
+    ] : // child is not structured the same as the parent
+    [
+    Key,
+    ...NestedPaths<Type[Key], [...Depth, 1]>
+    ] | [Key];
+}[Extract<keyof Type, string>] : [];
+
+type Join<T extends unknown[], D extends string> = T extends [] ? "" : T extends [string | number] ? `${T[0]}` : T extends [string | number, ...infer R] ? `${T[0]}${D}${Join<R, D>}` : string
+
+export type objectNotation<T> = {
+  [Property in Join<NestedPaths<T, []>, ".">]: Property extends keyof T ? T[Property] : never
+};
+
+/**
+ * Remove dot notation from object
+ *
+ * @param notationObj - Notation object
+ * @returns Object without notation
+ */
+export function notationToObject(notationObj: any): any {
+  const obj: any = {};
+  for (const key in notationObj) {
+    let currentObj = obj;
+    const keys = key.split(".");
+    for (let i = 0; i < keys.length; i++) {
+      if (i === keys.length - 1) currentObj[keys[i]] = notationObj[key];
+      else {
+        currentObj[keys[i]] ??= {};
+        currentObj = currentObj[keys[i]];
+      }
+    }
+  }
+  return obj;
+}
+
+/**
+ * Add notation to object
+ *
+ * @param obj - Object
+ * @returns Notation object
+ */
+export function objectToNotation<T>(obj: T): objectNotation<T> {
+  const notationObj: any = {};
+  function recursive(obj: any, notationObj: any, path: string) {
+    for (const key in obj) {
+      if (typeof obj[key] === "object") recursive(obj[key], notationObj, path + key + ".");
+      else notationObj[path + key] = obj[key];
+    }
+  }
+  recursive(obj, notationObj, "");
+  return {
+    ...obj,
+    ...notationObj
+  };
+}
