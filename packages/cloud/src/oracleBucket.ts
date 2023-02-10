9814593a9d5cfb2ddbce38970d374b43727fef08
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2023-02-09 22:46:14 -0300

    add watch to oracle bucket

--- packages/cloud/src/oracleBucket.ts
+++ packages/cloud/src/oracleBucket.ts
@@ -1,8 +1,12 @@
 import * as ociBucket from "oci-objectstorage";
 import * as ociAuth from "oci-common";
 import * as coreHttp from "@sirherobrine23/http";
+import * as Extends from "@sirherobrine23/extends";
+import { createReadStream, createWriteStream, promises as fs } from "node:fs";
+import chokidar from "chokidar";
 import stream from "node:stream";
 import utils from "node:util";
+import path from "node:path";
 
 export type oracleRegions = "af-johannesburg-1"|"ap-chuncheon-1"|"ap-hyderabad-1"|"ap-melbourne-1"|"ap-mumbai-1"|"ap-osaka-1"|"ap-seoul-1"|"ap-singapore-1"|"ap-sydney-1"|"ap-tokyo-1"|"ca-montreal-1"|"ca-toronto-1"|"eu-amsterdam-1"|"eu-frankfurt-1"|"eu-madrid-1"|"eu-marseille-1"|"eu-milan-1"|"eu-paris-1"|"eu-stockholm-1"|"eu-zurich-1"|"il-jerusalem-1"|"me-abudhabi-1"|"me-jeddah-1"|"mx-queretaro-1"|"sa-santiago-1"|"sa-saopaulo-1"|"sa-vinhedo-1"|"uk-cardiff-1"|"uk-london-1"|"us-ashburn-1"|"us-chicago-1"|"us-phoenix-1"|"us-sanjose-1";
 export type oracleOptions = {
@@ -67,19 +71,59 @@ function checkFileName(name: string) {
   return name;
 }
 
+type oracleFileListObjectinternal = {
+  "name": string,
+  "size"?: number,
+  "timeCreated"?: string,
+  "timeModified"?: string,
+  "etag"?: string,
+  "storageTier"?: "Standard"|"InfrequentAccess"|"Archive",
+  "archivalState"?: "Archived"|"Restoring"|"Restored",
+  "md5"?: string
+};
+
+export type oracleFileListObject = {
+  name: string,
+  size: number,
+  etag: string,
+  storageTier: "Standard"|"InfrequentAccess"|"Archive",
+  md5: string,
+  getFile: () => Promise<stream.Readable>,
+  Dates: {
+    Created: Date,
+    Modified: Date
+  },
+};
+
+export type oracleBucket = {
+  listFiles(folderPath?: string): Promise<oracleFileListObject[]>,
+  deleteFile(pathLocation: string): Promise<void>,
+  uploadFile(fileName: string, fileStream: string|Buffer|stream.Readable): Promise<void>,
+  getFileStream(pathLocation: string): Promise<stream.Readable>,
+  updateTier?(filePath: string, storageTier: "Standard"|"InfrequentAccess"|"Archive"): Promise<void>,
+  watch?(filePath: string, options?: {downloadFist?: boolean, remoteFolder?: string}): Promise<chokidar.FSWatcher>,
+};
+
 /**
  * Create object with functions to manage files in Oracle cloud bucket
  */
-export async function oracleBucket(config: oracleOptions) {
+export async function oracleBucket(config: oracleOptions): Promise<oracleBucket> {
+  const partialFunctions: Partial<oracleBucket> = {};
   if (config.auth.type === "preAuthentication") {
     if (!config.auth.PreAuthenticatedKey) throw new Error("PreAuthenticatedKey is required");
     getRegion(config.region);
     const baseURL = utils.format("https://objectstorage.%s.oraclecloud.com/p/%s/n/%s/b/%s", config.region, config.auth.PreAuthenticatedKey, config.namespace, config.name);
 
-    async function uploadFile(fileName: string, fileStream: string|Buffer|stream.Readable) {
+    function fixDir(filePath: string) {
+      filePath = path.posix.normalize(path.posix.resolve("/", filePath));
+      if (filePath.startsWith("/")) filePath = filePath.slice(1);
+      return filePath;
+    }
+
+    partialFunctions.uploadFile = async function uploadFile(fileName: string, fileStream: string|Buffer|stream.Readable) {
       await coreHttp.bufferRequest({
         method: "PUT",
-        url: utils.format("%s/o/%s", baseURL, encodeURIComponent(checkFileName(fileName))),
+        url: utils.format("%s/o/%s", baseURL, encodeURIComponent(fixDir(checkFileName(fileName)))),
         body: fileStream,
         headers: {
           "Content-Type": "application/octet-stream",
@@ -87,30 +131,19 @@ export async function oracleBucket(config: oracleOptions) {
       })
     }
 
-    async function deleteFile(pathLocation: string) {
+     partialFunctions.deleteFile = async function deleteFile(pathLocation: string) {
       await coreHttp.bufferRequest({
         method: "DELETE",
-        url: utils.format("%s/o/%s", baseURL, encodeURIComponent(checkFileName(pathLocation))),
+        url: utils.format("%s/o/%s", baseURL, encodeURIComponent(fixDir(checkFileName(pathLocation)))),
       })
     }
 
-    async function listFiles(folder?: string) {
-      const data: {
-        "name": string,
-        "size"?: number,
-        "timeCreated"?: string,
-        "timeModified"?: string,
-        "etag"?: string,
-        "storageTier"?: "Standard"|"InfrequentAccess"|"Archive",
-        "archivalState"?: "Archived"|"Restoring"|"Restored",
-        "md5"?: string
-      }[] = [];
+    partialFunctions.listFiles = async function listFiles(folder: string = "") {
+      folder = fixDir(folder);
+      const data: oracleFileListObject[] = [];
       let startAfter: string;
       while (true) {
-        const response = await coreHttp.jsonRequest<{
-          nextStartWith?: string,
-          objects: typeof data
-        }>({
+        const response = await coreHttp.jsonRequest<{nextStartWith?: string, objects: oracleFileListObjectinternal[]}>({
           method: "GET",
           url: utils.format("%s/o", baseURL),
           query: {
@@ -120,24 +153,27 @@ export async function oracleBucket(config: oracleOptions) {
             startAfter: startAfter ?? "",
           }
         });
-        data.push(...response.body.objects);
+        response.body.objects.forEach(item => data.push({
+          name: item.name,
+          size: item.size,
+          etag: item.etag,
+          storageTier: item.storageTier,
+          md5: item.md5,
+          getFile: () => partialFunctions!.getFileStream(item.name),
+          Dates: {
+            Created: new Date(item.timeCreated),
+            Modified: new Date(item.timeModified)
+          }
+        }));
         if (!(startAfter = response.body.nextStartWith)) break;
       }
-      return data.map((item) => ({
-        name: item.name,
-        size: item.size,
-        timeCreated: item.timeCreated ? new Date(item.timeCreated) : undefined,
-        timeModified: item.timeModified ? new Date(item.timeModified) : undefined,
-        storageTier: item.storageTier,
-        archivalState: item.archivalState,
-        md5: item.md5,
-      }));
+      return data;
     }
 
-    async function getFileStream(pathLocation: string): Promise<stream.Readable> {
+    partialFunctions.getFileStream = async function getFileStream(pathLocation: string): Promise<stream.Readable> {
       const response = await coreHttp.streamRequest({
         method: "GET",
-        url: utils.format("%s/o/%s", baseURL, encodeURIComponent(checkFileName(pathLocation))),
+        url: utils.format("%s/o/%s", baseURL, encodeURIComponent(fixDir(checkFileName(pathLocation)))),
       });
       return response;
     }
@@ -159,7 +195,7 @@ export async function oracleBucket(config: oracleOptions) {
     //   });
     // }
 
-    async function updateTier(filePath: string, storageTier: "Standard"|"InfrequentAccess"|"Archive") {
+    partialFunctions.updateTier = async function updateTier(filePath: string, storageTier: "Standard"|"InfrequentAccess"|"Archive") {
       if (!(["Standard", "InfrequentAccess", "Archive"]).includes(storageTier)) throw new TypeError("Invalid storage tier");
       await coreHttp.bufferRequest({
         method: "POST",
@@ -168,93 +204,108 @@ export async function oracleBucket(config: oracleOptions) {
           "Content-Type": "application/json",
         },
         body: {
-          objectName: checkFileName(filePath),
+          objectName: fixDir(checkFileName(filePath)),
           storageTier,
         }
       });
     }
-
-    return {
-      baseURL,
-      listFiles,
-      uploadFile,
-      deleteFile,
-      // renameFile,
-      updateTier,
-      getFileStream,
-    };
-  }
-  const client = new ociBucket.ObjectStorageClient({
-    authenticationDetailsProvider: new ociAuth.SimpleAuthenticationDetailsProvider(
-      config.auth.tenancy,
-      config.auth.user,
-      config.auth.fingerprint,
-      config.auth.privateKey,
-      config.auth.passphase||null,
-      getRegion(config.region)
-    )
-  });
-  async function uploadFile(fileName: string, fileStream: string|Buffer|stream.Readable) {
-    await client.putObject({
-      namespaceName: config.namespace,
-      bucketName: config.name,
-      objectName: fileName,
-      putObjectBody: fileStream,
+  } else {
+    const client = new ociBucket.ObjectStorageClient({
+      authenticationDetailsProvider: new ociAuth.SimpleAuthenticationDetailsProvider(
+        config.auth.tenancy,
+        config.auth.user,
+        config.auth.fingerprint,
+        config.auth.privateKey,
+        config.auth.passphase||null,
+        getRegion(config.region)
+      )
     });
-  }
 
-  async function deleteFile(pathLocation: string) {
-    await client.deleteObject({
-      namespaceName: config.namespace,
-      bucketName: config.name,
-      objectName: pathLocation
-    });
-  }
-
-  async function listFiles(folder?: string) {
-    const objects: ociBucket.models.ObjectSummary[] = [];
-    let start: any;
-    while (true) {
-      const { listObjects } = await client.listObjects({
+    partialFunctions.uploadFile = async function uploadFile(fileName: string, fileStream: string|Buffer|stream.Readable) {
+      await client.putObject({
         namespaceName: config.namespace,
         bucketName: config.name,
-        fields: "name,size,etag,timeCreated,md5,timeModified,storageTier,archivalState" as any,
-        prefix: folder,
-        startAfter: start,
+        objectName: fileName,
+        putObjectBody: fileStream,
       });
-      objects.push(...listObjects.objects);
-      if (!(start = listObjects.nextStartWith)) break;
     }
 
-    return objects.map(data => ({
-      path: data.name,
-      size: Number(data.size||0),
-      state: data.archivalState,
-      tier: data.storageTier,
-      md5: data.md5,
-      getFileStream: () => getFileStream(data.name),
-      dates: {
-        created: new Date(data.timeCreated),
-        modified: new Date(data.timeModified),
+    partialFunctions.deleteFile = async function deleteFile(pathLocation: string) {
+      await client.deleteObject({
+        namespaceName: config.namespace,
+        bucketName: config.name,
+        objectName: pathLocation
+      });
+    }
+
+    partialFunctions.listFiles = async function listFiles(folder?: string) {
+      const objects: oracleFileListObject[] = [];
+      let start: any;
+      while (true) {
+        const { listObjects } = await client.listObjects({
+          namespaceName: config.namespace,
+          bucketName: config.name,
+          fields: "name,size,etag,timeCreated,md5,timeModified,storageTier,archivalState" as any,
+          prefix: folder,
+          startAfter: start,
+        });
+        listObjects.objects.forEach(item => objects.push({
+          name: item.name,
+          size: item.size,
+          etag: item.etag,
+          storageTier: item.storageTier as any,
+          md5: item.md5,
+          getFile: () => partialFunctions!.getFileStream(item.name),
+          Dates: {
+            Created: new Date(item.timeCreated),
+            Modified: new Date(item.timeModified)
+          }
+        }))
+        if (!(start = listObjects.nextStartWith)) break;
       }
-    }));
+
+      return objects;
+    }
+
+    partialFunctions.getFileStream = async function getFileStream(pathLocation: string) {
+      const { value } = await client.getObject({
+        namespaceName: config.namespace,
+        bucketName: config.name,
+        objectName: pathLocation,
+      });
+      if (!value) throw new Error("No file found");
+      else if (value instanceof stream.Readable) return value;
+      else return stream.Readable.fromWeb(value as any);
+    }
   }
 
-  async function getFileStream(pathLocation: string) {
-    const { value } = await client.getObject({
-      namespaceName: config.namespace,
-      bucketName: config.name,
-      objectName: pathLocation,
+  partialFunctions.watch = async function(folderPath, options) {
+    if (!options) options = {};
+    if (!folderPath) throw new TypeError("Folder path is required");
+    else if (!(await Extends.exists(folderPath))) throw new Error("Folder path is not exists");
+    else if (!(await Extends.isDirectory(folderPath))) throw new Error("Folder path is not a directory");
+    if (options.downloadFist) {
+      let { remoteFolder = "" } = options;
+      const filesList = (await partialFunctions!.listFiles(remoteFolder)).map(d => d.name);
+      const localList = (await Extends.readdir(folderPath)).map(file => path.posix.resolve("/", path.relative(folderPath, file)));
+      for (const local of localList) if (!filesList.includes(local)) await fs.unlink(path.posix.resolve(folderPath, local));
+      for await (const remote of filesList) await new Promise(async (done, reject) => (await partialFunctions!.getFileStream(remote)).pipe(createWriteStream(path.posix.resolve(folderPath, remote))).on("error", reject).once("done", done));
+    }
+
+    return chokidar.watch(folderPath, {
+      ignoreInitial: true,
+      atomic: true,
+    }).on("add", async (filePath) => {
+      await partialFunctions!.uploadFile(path.posix.resolve("/", path.relative(folderPath, filePath)), createReadStream(filePath));
+    }).on("change", async (filePath) => {
+      await partialFunctions!.uploadFile(path.posix.resolve("/", path.relative(folderPath, filePath)), createReadStream(filePath));
+    }).on("unlink", async (filePath) => {
+      await partialFunctions!.deleteFile(path.posix.resolve("/", path.relative(folderPath, filePath)));
+    }).on("unlinkDir", async (filePath) => {
+      const filesList = (await partialFunctions!.listFiles(path.posix.resolve("/", path.relative(folderPath, filePath)))).map(d => d.name);
+      for await (const remote of filesList) await partialFunctions!.deleteFile(remote);
     });
-    if (!value) throw new Error("No file found");
-    else if (value instanceof stream.Readable) return value;
-    else return stream.Readable.fromWeb(value as any);
   }
 
-  return {
-    uploadFile,
-    deleteFile,
-    listFiles,
-    getFileStream,
-  };
+  return partialFunctions as oracleBucket;
 }
