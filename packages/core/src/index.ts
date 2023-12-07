2c2166d1bdcbf1a95ca7f37f1656baca9060c7bb
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2023-12-07 16:27:39 -0300

    core drop dpkg

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,6 @@
 export * as Ar from "@sirherobrine23/ar";
 export * as http from "@sirherobrine23/http";
 export * as Extends from "@sirherobrine23/extends";
-export * as Debian from "@sirherobrine23/dpkg";
-export * as Dpkg from "@sirherobrine23/dpkg";
 export * as Cloud from "@sirherobrine23/cloud";
 export * as Docker from "@sirherobrine23/docker-registry";
 export { M3U, parseM3u } from "./m3u.js";
